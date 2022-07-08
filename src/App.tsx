d13b46c0909e9c7efea955001e1b4ce26d06717a
kjg <kjg@iwi.co.kr>
2022-07-08 18:19:39 +0900

    7.5 Gestures part One

--- src/App.tsx
+++ src/App.tsx
@@ -12,45 +12,22 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 50%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  margin: 20px;
 `;
 
 const boxVariants = {
-  start: { opacity: 0, scale: 0.5 },
-  end: {
-    opacity: 1,
-    scale: 1,
-    transition: { type: "spring", duration: 0.5, bounce: 0.5, delayChildren: 0.25, staggerChildren: 0.1 },
-  },
-};
-
-const circleVariants = {
-  start: { opacity: 0, y: 10 },
-  end: { opacity: 1, y: 0 },
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "50%" },
+  drag: { backgroundColor: "rgba(46, 204, 113, 1)", transition: { duration: 0.5 } },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial='start' animate='end'>
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <Box drag variants={boxVariants} whileHover='hover' whileTap='click' whileDrag='drag' />
     </Wrapper>
   );
 }
