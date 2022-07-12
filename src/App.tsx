c7cff3506947b5e763f5b564a24f3a061a3e59eb
kjg <kjg@iwi.co.kr>
2022-07-12 14:21:46 +0900

    7.9 MotionValues part Three + @

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 import { motion, useMotionValue, useTransform, useViewportScroll } from "framer-motion";
-import { useEffect } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 200vh;
-  width: 100vw;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -13,32 +12,41 @@ const Wrapper = styled(motion.div)`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 1);
+  background-color: rgba(255, 255, 255, 0.3);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  position: fixed;
+  left: calc(50% - 100px);
+  top: calc(50% - 100px);
+  overflow: hidden;
+  display: flex;
+  align-items: end;
+`;
+const Fill = styled(motion.div)`
+  position: relative;
+  background: white;
+  width: 100%;
 `;
 
 function App() {
   const x = useMotionValue(0);
   const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const { scrollYProgress } = useViewportScroll();
   const gradient = useTransform(
-    x,
-    [-800, 800],
+    scrollYProgress,
+    [0, 1],
     [
       "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238)",
       "linear-gradient(135deg, rgb(0, 238, 155), rgb(238,178,0))",
     ]
   );
-  const { scrollY, scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-  useEffect(() => {
-    scrollY.onChange(() => {
-      console.log(scrollY.get(), scrollYProgress.get());
-    });
-  }, [scrollY, scrollYProgress]);
+  const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+  const height = useTransform(scrollYProgress, [0, 1], ["0", "100%"]);
   return (
     <Wrapper style={{ background: gradient }}>
-      <Box style={{ x, rotateZ, scale }} drag='x' dragSnapToOrigin />
+      <Box style={{ x, rotateZ, scale }}>
+        <Fill style={{ height }} />
+      </Box>
     </Wrapper>
   );
 }
