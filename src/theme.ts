acb3f53c86449e999aa155c6e00939ba7d4d15de
kjg <kjg@iwi.co.kr>
2022-07-13 16:50:12 +0900

    8.0 Introduction

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,14 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {};
+export const theme: DefaultTheme = {
+  red: "#e51013",
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2f2f2f",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#e5e5e5",
+  },
+};
