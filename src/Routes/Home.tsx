f0a7aed5a1c4e6d0a39eedca50250a18076651a0
kjg <kjg@iwi.co.kr>
2022-07-15 09:36:49 +0900

    8.6 Home Screen part Two

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,10 +1,65 @@
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import styled from "styled-components";
+import { getMovies, IGetMoviesResult } from "../api";
+import { makeImagePath } from "../utils";
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 50px;
+  margin-bottom: 20px;
+  letter-spacing: -0.1em;
+`;
+
+const Overview = styled.p`
+  font-size: 16px;
+  line-height: 24px;
+  width: 50%;
+  height: 72px;
+  display: -webkit-box;
+  word-wrap: break-word;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  letter-spacing: -0.05em;
+`;
 
 function Home() {
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
-  console.log(data, isLoading);
-  return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>;
+  const { data, isLoading } = useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "") || ""}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,22 @@
 const LANGUAGE = "ko-KR";
 const API_KEY = "92d2be67030e2606ed1abba7da76fb7a";
-
 const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: { minimum: string; maximum: string };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
 
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}&language=${LANGUAGE}`).then((response) =>
